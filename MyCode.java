48138f58e7f100cbbdc29c2f0f4bb8de2f4acd8a
Qasim Baig <zhac215@live.rhul.ac.uk>
2020-11-27 16:53:07 +0000

    basic template for program

--- MyCode.java
+++ MyCode.java
@@ -1,3 +1,21 @@
+import java.util.Scanner
+static Scanner kbd = new Scanner(System.in);
 public class Mycode{
-    System.out.println("testing github")
+    public static void main (String [] args){
+    System.out.println("testing github");
+    boolean exit = false;
+        while (!exit){
+        System.out.println("1. Edit mode");
+        System.out.println("2. Analysis mode");
+        int choice = kbd.nextInt();
+            if (choice == 1){
+              // do something with classes
+            }
+            if (choice == 2){
+              // something with edit class
+            }
+
+
+        }
+    }
 }
