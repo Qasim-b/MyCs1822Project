b1191cd5b99139bbadafcf02935b7fa8276a9d9e
Qasim Baig <zhac215@live.rhul.ac.uk>
2020-11-27 17:03:01 +0000

    added task requirement comments

--- MyCode.java
+++ MyCode.java
@@ -19,3 +19,18 @@ public class Mycode{
         }
     }
 }
+
+//Edit mode
+//Analysis mode
+//In edit mode, users can add and delete expense items. An expense item should include
+
+//Description
+//Category
+//Amount
+//Exiting edit mode should return the user to the initial menu.
+
+//In analysis mode, the user will be shown summary data of the expenses they have entered. This will include
+
+//Breakdown of expenditure by category.
+//Average spend per item.
+//Total expenditure.
