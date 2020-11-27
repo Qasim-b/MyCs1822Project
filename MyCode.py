5fe3e18daf64e45baec6e7d9dbaab91ce4c22347
Qasim Baig <zhac215@live.rhul.ac.uk>
2020-11-27 19:12:23 +0000

    created basic menu. edit function creates a csv that stores the fields.

--- MyCode.py
+++ MyCode.py
@@ -1,16 +1,28 @@
+import csv
+def editMode():
+    with open('expenses.csv', 'w', newline='') as file: #probs need to check if exists aswell.
+        writer = csv.writer(file)
+        loop = True
+        ID = 0
+        while (loop):
+            Description = input("Enter a description")
+            Category = input("Enter a category")
+            Amount = int(input("Enter an amount"))
+            writer.writerow(["ID","Description", "Category", "Amount"])
+            writer.writerow([ID, Description, Category, Amount])
+            ID = ID + 1
+            usexit = input("add another entry? y/n")
+            if usexit == "n":
+                loop = False
+                menu()
 
+def menu():
+    print("""Menu:
+1. Edit mode
+2. Analysis mode""")
+    choice = int(input("Choose either option '1' or '2' "))
+    if choice == 1:
+        editMode()
 
-//Edit mode
-//Analysis mode
-//In edit mode, users can add and delete expense items. An expense item should include
-
-//Description
-//Category
-//Amount
-//Exiting edit mode should return the user to the initial menu.
-
-//In analysis mode, the user will be shown summary data of the expenses they have entered. This will include
-
-//Breakdown of expenditure by category.
-//Average spend per item.
-//Total expenditure.
+if __name__ == "__main__":
+    menu()
