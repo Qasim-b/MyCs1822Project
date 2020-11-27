ab18ab7906f81793f5df480facae1c1fa8b16290
TyTrem <75134355+TyTrem@users.noreply.github.com>
2020-11-27 23:46:43 +0000

    Analysis Functionality

--- MyCode.py
+++ MyCode.py
@@ -16,6 +16,56 @@ def editMode():
                 loop = False
                 menu()
 
+def totalExpenditure(listOfExpenditure):
+	total = 0
+
+	for n in listOfExpenditure:
+		total += n
+
+
+	return total
+
+def avgExpenditure(listOfExpenditure):
+
+    total = 0
+    nItems = 0
+
+    for n in listOfExpenditure:
+    	total += n
+    	nItems += 1
+
+
+    return total / nItems
+
+
+def performAnalysis():
+
+	totalExpenditure = 0
+	nExpenses = 0
+	catagories = {}
+
+	with open('expenses.csv', 'r', newline='') as expenses:
+		reader = csv.DictReader(expenses)
+		for record in reader:
+				# print(record['Description'], record['Category'], record['Amount'])
+				totalExpenditure += int(record['Amount'])
+				if (record['Category'] not in catagories):
+					catagories[record['Category']] = int(record['Amount'])
+				elif (record['Category'] in catagories):
+					catagories[record['Category']] += int(record['Amount'])
+				nExpenses += 1
+
+	averageExp = totalExpenditure / nExpenses
+
+
+	print("\nExpenditure by catagory:")
+	for key in catagories:
+		print(key, " : ", catagories[key])
+
+	print("\nAverage Expenditure: " + str(averageExp))
+
+	print("\nTotal Expenditure: " + str(totalExpenditure))
+
 def menu():
     print("""Menu:
 1. Edit mode
@@ -23,6 +73,8 @@ def menu():
     choice = int(input("Choose either option '1' or '2' "))
     if choice == 1:
         editMode()
+    elif choice = 2:
+        performAnalysis()
 
 if __name__ == "__main__":
     menu()
