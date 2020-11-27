5f6305c3d575c7299bdc8d160d633d9b8f4da4ad
Qasim Baig <zhac215@live.rhul.ac.uk>
2020-11-27 17:12:43 +0000

    python

---
+++ MyCode.py
@@ -0,0 +1,16 @@
+
+
+//Edit mode
+//Analysis mode
+//In edit mode, users can add and delete expense items. An expense item should include
+
+//Description
+//Category
+//Amount
+//Exiting edit mode should return the user to the initial menu.
+
+//In analysis mode, the user will be shown summary data of the expenses they have entered. This will include
+
+//Breakdown of expenditure by category.
+//Average spend per item.
+//Total expenditure.
