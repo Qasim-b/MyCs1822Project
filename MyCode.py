bb2658239109850789d9a3434e8cd7421ee6cc74
Qasim Baig <zhac215@live.rhul.ac.uk>
2020-11-28 23:16:42 +0000

    implement deleting entries, improved program traversal

--- MyCode.py
+++ MyCode.py
@@ -1,8 +1,26 @@
 import csv
 import os.path
+def choice_Delete():
+    loop = True
+    while (loop):
+        entrydelete = input("Enter the description of the entry you would like to delete")
+        with open('expenses.csv', 'r+') as file_del:
+            fieldnames = ["Description","Category","Amount"]
+            reader = csv.DictReader(file_del, fieldnames)
+            filtered_output = [line for line in reader if line['Description'] != entrydelete]
+            file_del.seek(0)
+            writer = csv.DictWriter(file_del, fieldnames)
+            writer.writerows(filtered_output)
+            file_del.truncate()
+            print("Entry successfully deleted")
+            delchoice = int(input("Would you like to delete another entry? 1.yes / 2.no"))
+            if delchoice == 2:
+                loop = False
+                editMode()
+
 def editMode():
     if os.path.isfile("expenses.csv"):
-        filechoice = int(input("1.add data / 2.delete data "))
+        filechoice = int(input("1.add data / 2.delete data / 3.Return to menu "))
         if filechoice == 1:
             with open('expenses.csv', 'a', newline='') as file: #probs need to check if exists aswell.
                 fieldnames = ["Description","Category","Amount"]
@@ -18,7 +36,9 @@ def editMode():
                         loop = False
                         menu()
         elif filechoice == 2:
-            deletechoice = input("enter the decription of the item you would like to delete")
+            choice_Delete()
+        elif filechoice == 3:
+            menu()
             #program will search file for the line, will open 2 files. 2nd one called expenses_edit
             #program will then add everything barring the line thats chosen, then will delete expenses
             #will then rename expenses_edit into expenses.
